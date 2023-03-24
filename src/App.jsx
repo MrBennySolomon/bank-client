4d214b7e34de69241a190ba53e25a9efe3985932
בני סולומון <mr.benny.solomon@gmail.com>
2023-03-24 20:22:57 +0300

    first client commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,32 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [users, setUsers] = useState([]);
+  
+  const fetchUsers = async () => {
+    const users = await axios.get('https://bank-mernstack.onrender.com/api/v1/users');
+    setUsers(users.data);
+  }
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <table>
+        <thead><td>Name</td><td>Cash</td><td>Credit</td><td>Actions</td></thead>
+        <tbody>
+          <tr>
+            { users.map((user) => <><td key={user._id}>user.name</td><td>user.cash</td><td>user.credit</td><button onClick={depositeHandler}>Deposite</button><button onClick={withdrawHandler}>Withdraw</button><button onClick={transferHandler}>Transfer</button></>)}
+          </tr>
+        </tbody>
+        
+      </table>
     </div>
   )
 }
